1c624fc1d45d960e25b292dfcc48db0afd02da15
VanLien1503 <hoanglien89nd@gmail.com>
2020-05-07 21:30:07 +0700

    Thucj Hanhf ComParator

---
+++ ShapeTest.java
@@ -0,0 +1,11 @@
+package TH_TrienKhai_Comparator;
+
+class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape=new Shape();
+        System.out.println(shape);
+
+        shape=new Shape("red", false);
+        System.out.println(shape);
+    }
+}
